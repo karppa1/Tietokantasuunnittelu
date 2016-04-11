91e482359ac4afbc5bca7d682fde7a14f28097a9
K2961 <aksim91@gmail.com>
2016-04-11 15:27:59 +0300

    Update HarjoitustyoDummyData.sql

--- HarjoitustyoDummyData.sql
+++ HarjoitustyoDummyData.sql
@@ -1,3 +1,27 @@
-INSERT INTO Cargo (CompanyID, CarrierID, Arrived, SignerID, Signed, Signature) VALUES 
-(2138491, 1423, NULL , 5315, 'Y', NULL),
-(1512424, 5215, NULL, 8542, 'Y', NULL);
+INSERT INTO Group (ID) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6);
+
+INSERT INTO Person (GroupID, Name) VALUES
+(3, Make Makkonen),
+(2, Nakke Nakuttaja);
+
+INSERT INTO Cargo (ID, CompanyID, CarrierID, Arrived, SignerID, Signed, Signature) VALUES 
+(1, 2138491, 1423, NULL , 5315, 'Y', NULL),
+(2, 1512424, 5215, NULL, 8542, 'Y', NULL),
+(3, 6264262, 6548, NULL, 8786, 'Y', NULL);
+
+INSERT INTO SpecialCargo (ID) VALUES
+(1);
+
+INSERT INTO Reservation (ID, CargoID, Description) VALUES
+(1, 3, 'Särkyvää');
+
+INSERT INTO CargoItem (ID, CargoID, Type, Content, Number) VALUES
+(1, 1, 'EURO', 'Ydinjäte', 20),
+(2, 2, 'EURO', 'Toimistotarvikkeet', 15),
+(3, 3, 'BARREL', 'Öljyä', 20);
