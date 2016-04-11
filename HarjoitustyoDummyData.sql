4056f639e1f8df7a22ed32bad41d9521c6c519be
karppa1 <K2264@student.jamk.fi>
2016-04-11 15:40:09 +0300

    Update HarjoitustyoDummyData.sql

--- HarjoitustyoDummyData.sql
+++ HarjoitustyoDummyData.sql
@@ -6,15 +6,19 @@ INSERT INTO PersonGroup (ID) VALUES
 (5),
 (6);
 
+INSERT INTO Company (ID,Name) VALUES
+(2138491,'Rahti Oy'),
+(1512424,'Pikakuljetus');
+
 INSERT INTO Person (ID, GroupID, Name) VALUES
 (1, 3, 'Make Makkonen'),
 (2, 2, 'Nakke Nakuttaja'),
 (3, 3, 'Seko Seppo');
 
 INSERT INTO Cargo (ID, CompanyID, CarrierID, Arrived, SignerID, Signed, Signature) VALUES 
-(1, 2138491, 1, NULL , 1, 'Y', NULL),
-(2, 1512424, 1, NULL, 1, 'Y', NULL),
-(3, 6264262, 2, NULL, 2, 'Y', NULL);
+(1, 2138491, 1, NULL , 3, 'Y', NULL),
+(2, 1512424, 1, NULL, 3, 'Y', NULL),
+(3, 1512424, 2, NULL, 3, 'Y', NULL);
 
 INSERT INTO SpecialCargo (ID) VALUES
 (1);
@@ -26,4 +30,3 @@ INSERT INTO CargoItem (ID, CargoID, Type, Content, Number) VALUES
 (1, 1, 'EURO', 'Ydinjäte', 20),
 (2, 2, 'EURO', 'Toimistotarvikkeet', 15),
 (3, 3, 'BARREL', 'Öljyä', 20);
-
